824090981ef7a80f56b59ca81099fb3a1dde6a8e
Developer-Zahid <coderexpertzahid@gmail.com>
2025-02-26 21:36:29 +0600

    Lazy-Load-Videos init

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+var Webflow = Webflow || [];
+Webflow.push(function () {
+// End
+});
+
+
+/* Lazy Load Videos Functions */
+if ($('figure[data-lazy-video-src]').length > 0) {
+
+    $('figure[data-lazy-video-src]').each(function() {
+        const $figure = $(this);
+        const videoSrc = $figure.attr('data-lazy-video-src');
+        const videoType = $figure.attr('data-lazy-video-type') || 'mp4';
+        const $posterImage = $figure.find('img');
+        const $posterImageClass = $posterImage.attr('class');
+        function replaceImageWithVideo() {
+            console.log("Poster Image Loaded, Replacing with Video");
+            const $highResVideo = $('<video>', {
+                autoplay: true,
+                muted: true,
+                loop: true,
+                playsinline: true,
+                preload: 'none',
+                fetchpriority: 'low',
+                decoding: 'async',
+                class: $posterImageClass,
+            });
+            $highResVideo.prop('muted', true);
+            const $videoSource = $('<source>', {
+                src: videoSrc,
+                type: `video/${videoType}`
+            });
+            $highResVideo.append($videoSource);
+            $highResVideo.on('loadeddata', function() {
+                $posterImage.remove(); // Remove the low-quality image
+                $figure.append($highResVideo); // Append the <video> to the <figure>
+                $figure.removeAttr('data-lazy-video-src'); // Optionally remove the attribute
+                console.log("Video Loaded and Rendered");
+            }).on('error', function() {
+                console.error("Video failed to load.");
+            });
+            // Start loading the video data manually
+            $highResVideo[0].load();
+        };
+        // Check if the image has already loaded
+        if ($posterImage[0].complete) {
+            replaceImageWithVideo();
+        } else {
+            // If the image is not yet loaded, add an event listener
+            $posterImage.on('load', replaceImageWithVideo);
+        }
+    });
+    
+};
